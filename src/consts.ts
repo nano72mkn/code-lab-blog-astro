f048d2058f1b8b7d26a59b422b738c4c129172e0
shota.miyahira <shota.miyahira@stafes.com>
2023-07-22 01:32:07 +0900

    SITE_DESCRIPTION変更

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Code Lab 🤔";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "フロントエンジニア、@nano72mknの個人ブログ&実験サイト";
