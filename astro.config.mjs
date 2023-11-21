2d051c551c705df2886f34471ab7727abed95c10
Shota Miyahira <2525.natuta@gmail.com>
2023-11-22 01:37:44 +0900

    staticに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
 import remarkBreaks from "remark-breaks";
 import remarkCodeTitle from "remark-flexible-code-titles";
