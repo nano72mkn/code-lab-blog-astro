ccaa0d1bb9013087f7f949ac11a243e3c95a0066
Shota Miyahira <2525.natuta@gmail.com>
2023-11-21 20:25:09 +0900

    フォントの追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"M PLUS Rounded 1c"', ...defaultTheme.fontFamily.sans],
+      },
       aspectRatio: {
         ogImage: "1.91 / 1",
       },
