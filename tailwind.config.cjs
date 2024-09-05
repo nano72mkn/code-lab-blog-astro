182ff59882b77826a23d4322bc97512427923ef0
shota.miyahira <shota.miyahira@stafes.com>
2024-09-05 12:48:05 +0900

    aboutページ

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,68 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: [
-          '"Noto Sans JP"',
-          '"Noto Color Emoji"',
+          '"Noto Sans JP Variable"',
           ...defaultTheme.fontFamily.sans,
         ],
       },
       aspectRatio: {
         ogImage: "1.91 / 1",
       },
+      animation: {
+        "tracking-in-expand": "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
+        "about-card": "about-card 5s both",
+        "about-card-icon": "about-card-icon 5s both",
+      },
+      keyframes: {
+        "tracking-in-expand": {
+          "0%": {
+            "letter-spacing": "-.5em",
+            opacity: "0"
+          },
+          "40%": {
+            opacity: ".6"
+          },
+          to: {
+            opacity: "1"
+          }
+        },
+        "about-card": {
+          "0%": {
+            height: "0",
+            width: "0",
+          },
+          "10%": {
+            height: "5px",
+            width: "10px",
+          },
+          "30%": {
+            height: "5px",
+            width: "400px",
+          },
+          "90%": {
+            height: "5px",
+            width: "400px",
+          },
+          "100%": {
+            height: "250px",
+            width: "400px",
+          }
+        },
+        "about-card-icon": {
+          "0%": {
+            height: "0",
+            width: "0",
+          },
+          "90%": {
+            height: "0",
+            width: "0",
+          },
+          "100%": {
+            height: "150px",
+            width: "150px",
+          }
+        }
+      }
     },
   },
   plugins: [],
